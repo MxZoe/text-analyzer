500ada8ea8102c2442c327f26d3958b0cfb38e1b
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-11 15:51:58 -0700

    added all bullet point functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,18 @@ function noInputtedText(text){
     return text.trim().length === 0;
 }
 
-function wordMatch(wordOne, wordTwo) {
-  return wordOne.toLowerCase().includes(wordOne.toLowerCase()) && wordOne.toLowerCase() === wordTwo.toLowerCase()
+function removePunctuation(text){
+  const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  text = text.replace(regex, '');
+  return text;
 }
+function wordMatch(wordOne, wordTwo) {
+  wordOne = removePunctuation(wordOne);
+  wordTwo = removePunctuation(wordTwo);
+
+  return wordOne.toLowerCase().includes(wordOne.toLowerCase()) && wordOne.toLowerCase() === wordTwo.toLowerCase();
+}
+
 //business logic
 
 function wordCounter(text){
@@ -49,7 +58,12 @@ function boldPassage(word, text) {
   textArray.forEach(function(element, index) {
     if (wordMatch(element, word)) {
       htmlString = htmlString.concat("<b>" + element + "</b>");
-    } else {
+    } 
+    else if(element.includes(word) && element !== word){
+      newElement = element.replace(word, "<b>" + word +"</b>");
+      htmlString = htmlString.concat(newElement);
+    }
+    else {
       htmlString = htmlString.concat(element);
     }
     if (index !== (textArray.length - 1)) {
@@ -72,7 +86,8 @@ function topThreeWords(text){
   let thirdWord = "";
   let result = "";
   wordArray.forEach(function(element){
-    if(countSentence.toLowerCase().includes(element.toLowerCase())){
+    let wordWithSpaces = " " + element + " ";
+    if(countSentence.toLowerCase().includes(wordWithSpaces.toLowerCase())){
       return
     }
     let numberOfTimes = numberOfOccurrencesInText(element, text);
@@ -86,13 +101,34 @@ function topThreeWords(text){
       thirdMost = numberOfTimes;
       thirdWord = element;
     }
-    
+
+
     result = "<br>" + topmostWord + " " + topmost + "<br>" + secondWord + " " + secondMost + "<br>" + thirdWord + " " +thirdMost;
     countSentence = countSentence.concat(result);
   });
   
   return result;
 }
+
+function censor(text){
+  const badWords = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+  if(noInputtedText(text)){
+    return 0;
+  }
+  const wordArray = text.trim().split(" ");
+  let censoredSentence = text;
+  badWords.forEach(function(element){
+    wordArray.forEach(function(word){
+      if(wordMatch(word, element)){
+        censoredSentence = censoredSentence.replace(word, '');    
+      }else {
+        return;
+      }
+    });
+  });
+  return censoredSentence;
+}
+
 
 // UI Logic
 
@@ -102,6 +138,7 @@ $(document).ready(function(){
     let passage = $("#text-passage").val();
     let word = $("#word").val();
     let wordCount = wordCounter(passage);
+    passage = censor(passage);
     let occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     let topWord = topThreeWords(passage);
     $("#total-count").html(wordCount);
